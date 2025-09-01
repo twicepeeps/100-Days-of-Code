f39a1e873a35ff355e4941b040ad55aee1014dfd
Bhavya <80588598+drtgszrykkkkkkk@users.noreply.github.com>
2025-09-01 16:46:04 +0530

    Create Q3.c

---
+++ Day01-10/Q3.c
@@ -0,0 +1,14 @@
+/* Q4-> program in c to calculate the area and circumference of a circle given its radius.
+*/
+#include <stdio.h>
+#define PI 3.14159
+int main(){
+  float radius , area , circumference;
+  printf("Enter the radius of the circle: ");
+  scanf("%f" , &radius);
+  area = PI*radius*radius;
+  circumference = 2*PI*radius;
+  printf("Area = %.2f\n" , area);
+  printf("Circumference = %.2f" , circumference);
+  return 0;
+}
