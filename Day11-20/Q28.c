a3e7240fdb501cc113f660d747798b0207892a2e
Bhavya <80588598+twicepeeps@users.noreply.github.com>
2025-09-21 22:58:49 +0530

    Update Q28.c

--- Day11-20/Q28.c
+++ Day11-20/Q28.c
@@ -5,11 +5,11 @@ Q28: Write a program to print the product of even numbers from 1 to n.
 
 int main() {
     int n;
-    long long product = 1;  // use long long to handle big numbers
+    long long product = 1;
     scanf("%d", &n);
 
     for (int i = 2; i <= n; i += 2) {
-        product *= i;   // multiply only even numbers
+        product *= i;   
     }
 
     printf("%lld", product);
