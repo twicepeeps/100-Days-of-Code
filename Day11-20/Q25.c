d8473e77a53c84add0fad8e48a208e3eea5ebfdb
Bhavya <80588598+twicepeeps@users.noreply.github.com>
2025-09-21 22:44:59 +0530

    Create Q25.c

---
+++ Day11-20/Q25.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char c;
+
+    scanf("%d %d %c", &a, &b, &c);
+    
+    switch (c) {
+        case '+':
+            printf("%d\n", a + b);
+            break;
+        case '-':
+            printf("%d\n", a - b);
+            break;
+        case '*':
+            printf("%d\n", a * b);
+            break;
+        case '/':
+            if (b != 0)
+                printf("%d\n", a / b);
+            else
+                printf("Error: Division by zero\n");
+            break;
+        case '%':
+            if (b != 0)
+                printf("%d\n", a % b);
+            else
+                printf("Error: Modulo by zero\n");
+            break;
+        default:
+            printf("Error: Invalid operator\n");
+    }
+
+    return 0;
+}
