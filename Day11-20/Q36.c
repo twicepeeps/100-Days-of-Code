520f01229e6df13a5318bb2215e99c0180b8f7e3
Bhavya <80588598+twicepeeps@users.noreply.github.com>
2025-09-23 18:45:14 +0530

    Create Q36.c

---
+++ Day11-20/Q36.c
@@ -0,0 +1,18 @@
+/*
+Q36: Write a program to find the HCF (GCD) of two numbers.
+*/
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("%d\n", a);
+    return 0;
+}
