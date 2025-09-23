3b418ec7578d0c6b0e7a896737feb5ac826bc851
Bhavya <80588598+twicepeeps@users.noreply.github.com>
2025-09-23 23:24:16 +0530

    Create Q62.c

---
+++ Day 31-40/Q62.c
@@ -0,0 +1,25 @@
+/*
+Q62: Reverse an array without taking extra space.
+*/
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n/2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n-1-i];
+        arr[n-1-i] = temp;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
