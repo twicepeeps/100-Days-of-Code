36f1eae4969a8de73e2a923c810c9aa2b1dbc65f
Bhavya <80588598+twicepeeps@users.noreply.github.com>
2025-09-29 15:38:49 +0530

    Create Q74.c

---
+++ Day 31-40/Q74.c
@@ -0,0 +1,18 @@
+/*
+Q74: Find the transpose of a matrix.
+*/
+#include <stdio.h>
+int main() {
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int a[r][c];
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+    for(int j=0;j<c;j++) {
+        for(int i=0;i<r;i++)
+            printf("%d ",a[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
