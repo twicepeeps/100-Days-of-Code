136b32d9dbf5c21f59720d526fb1590a6cef5850
Bhavya <80588598+twicepeeps@users.noreply.github.com>
2025-10-15 23:45:43 +0530

    Create Q61.c

---
+++ Day 31-40/Q61.c
@@ -0,0 +1,28 @@
+/*
+Q61: Search for an element in an array using linear search.
+*/
+#include <stdio.h>
+
+int main() {
+    int n, key, index = -1;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &key);
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index != -1)
+        printf("Found at index %d\n", index);
+    else
+        printf("-1\n");
+
+    return 0;
+}
